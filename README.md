688a8f1f2bf7a7d49cb1cd96d4a4d9ca0dd0a457
keripukki <arttu.kosonen@wsp.com>
2019-04-03 13:33:24 +0300

    client-only version

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# Transit display
+# Transit display for [KSO](http://kso.fi)
 
-A [Flask](http://flask.pocoo.org/) app showing selected transit departures requested regularly from [Digitransit](https://digitransit.fi/en/developers) API.
+Shows a selected amount of real-time or scheduled departures on selected bus or tram stops. A client-only application: just open `index.html` in your browser. The departures are fetched every minute by an AJAX request from [Digitransit Routing API](https://digitransit.fi/en/developers/apis/1-routing-api/).
 
-![Example image](media/example.png)
+See `scripts.js` for main functionality and modify the constants in the beginning if needed, i.e., to adjust the amount of departures to request and show.
 
-Python 3.X as well as Flask need to be installed. To run the app using localhost, follow the instructions of [Flask by Example](https://realpython.com/flask-by-example-part-1-project-setup/) tutorial.
+*Author:* Arttu Kosonen
 
-GraphQL request body to be sent to Digitransit API is defined in the beginning of `app.py` in dictionary named `conf`: you should pay attention to `stops(ids: [...])` and `numberOfDepartures` if you want to change the contents. Other body parameters are needed by further functions just as they are and should not be changed. `conf["start_offset"]` defines in seconds how close departures we don't want to request anymore: it might not be reasonable to show departures you can't reach due to 3-4 minutes walk anyway. `conf["max_departures_to_show"]` determines the amount of rows to which the displayed response is truncated, and you should adjust this so that all the rows fit into your screen. `conf["near_limit"]` defines time limit in minutes within which departure times are shown as "minutes left" instead of "HH:MM". `conf["about_to_depart"]` defines time limit in minutes within which minutes left will blink as the vehicle is just about to depart.
-
-In the current version, `BUS` and `TRAM` are the only assumed modes. The departures are divided into two spans accordingly. **TODO**: It is planned to have departures of all modes shown together sorted by departure time. **TODO**: Transit mode icons and space reserved for stop location descriptions seem to be useless if there is a narrow and well-known selection of stops and modes are represented in route name background colors – remove these from the layout.
-
-Request is made and departures are refreshed every minute: this behavior is defined in `static/scripts.js`.
-
-## Known issues / to be fixed soon
-- Departure times around midnight are not handled coherently and behave oddly: they do not sort right and negative values are shown after string formatting. Digitransit uses time values beyond 86400 seconds (> 24 hours) with night departures, and these are currently taken into account but then mix up e.g. with small int values of early morning departures.
-- On errors, nothing reasonable currently happens. If request fails or response has unexpected format, error text should be shown instead of old departure information left to display.
+4/2019
---
+++ scripts.js
@@ -0,0 +1,189 @@
+/*
+Stop times request to Digitransit API to be made once a minute
+and the HH:MM:SS clock.
+See https://digitransit.fi/en/developers/apis/1-routing-api/stops/
+for endpoint and request details.
+Alternative queries as well as further stops
+can be explored using GraphiQL:
+https://digitransit.fi/en/developers/apis/1-routing-api/1-graphiql/
+
+author: Arttu K
+3/2019
+*/
+
+const ENDPOINT = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+
+// Change stop selection and number of departures here if needed.
+const REQBODY = `{
+                    stops(ids: ["HSL:1130438", "HSL:1130110"]) {
+                        code
+                        lat
+                        lon
+                        stoptimesWithoutPatterns
+                          (startTime: START_TIME_PLACEHOLDER,
+                          numberOfDepartures: 10) {
+                        realtimeDeparture
+                        realtime
+                        trip {
+                            route {
+                            shortName
+                            mode
+                            }
+                        }
+                        headsign
+                        }
+                    }
+                }`;
+
+// Limit number of departures to show;
+// this is NOT the same as numberOfDepartures above!
+// numberOfDepartures is requested per-stop.
+const NDEPS = 10;
+
+// Departures are requested from +OFFSET_SEC seconds
+// from the moment the request is sent
+const OFFSET_SEC = 180;
+
+// Departures closer than NEAR_SEC are shown with blinking minutes left,
+// instead of static HH:MM.
+// Time difference is calculated when the request response is parsed.
+const NEAR_SEC = 600;
+
+function zpad(nr) {
+  // Zero padding for single-digit numbers in clock times
+    if (nr < 10) {
+      nr = '0' + nr
+    }
+    return nr;
+  }
+
+function formatDepTime(secs, nowseconds, realtime) {
+  // Format Digitransit-seconds into desired output
+  let time_part = "";
+  let tilde_part = "";
+  if (realtime === false) {
+    tilde_part = "~";
+  };
+  if (secs >= 86400) {
+    secs = secs - 86400;
+  };
+  let diff = Math.abs(secs - nowseconds);
+  //console.log(diff, nowseconds); // REMOVETHIS
+  let isnear = false;
+  if (diff <= NEAR_SEC) {
+    tilde_part = '<td class="notrealtime_sign near">' + tilde_part + "</td>";
+    isnear = true;
+    time_part += '<td class="dep_time_str near">' + Math.floor(diff / 60) + " min" + "</td>";
+  } else {
+    tilde_part = '<td class="notrealtime_sign">' + tilde_part + '</td>';
+    let hourpart = Math.floor(secs / 3600)
+    time_part += '<td class="dep_time_str">' + zpad(hourpart) + ":" + zpad(Math.floor((secs/60) - hourpart*60)) + "</td>";
+  };
+  return tilde_part + time_part;
+};
+
+function renderDepRow(dep, nowseconds) {
+  try {
+  // Render a <tr> for departure object
+  let output = "<tr>";
+
+  // Line number / short name
+  output += '<td class="route"><div class="route_short_name ' + dep.trip.route.mode.toLowerCase();
+  output += '">' + dep.trip.route.shortName + "</div></td>";
+
+  // Headsign
+  output += '<td class="trip_headsign">' + dep.headsign + '</td>';
+
+  // Departure time
+  output += formatDepTime(dep.realtimeDeparture, nowseconds, dep.realtime);
+  output += "</tr>\n";
+  return output;
+  } catch(e) {
+    console.log(e);
+    return "";
+  };
+};
+
+function compare(a, b) {
+  // For sorting departure objects by departure times in seconds
+  return a.realtimeDeparture - b.realtimeDeparture;
+};
+
+function renderDepartures(resp) {
+  // Function for handling Digitransit API departures JSON response;
+  // if there is anything wrong with the whole JSON object,
+  // an error text is rendered instead of the table.
+  try {
+    let stops = resp["data"]["stops"];
+    let departures = [];
+    let time = new Date();
+    let nowseconds = time.getHours() * 60 * 60 + time.getMinutes() * 60 + time.getSeconds();
+    for (let i = 0; i < stops.length; i++) {
+      let stopcode = stops[i]["code"];
+      let stoptimes = stops[i]["stoptimesWithoutPatterns"];
+      for (let j = 0; j < stoptimes.length; j++) {
+        let dep = stoptimes[j];
+        dep["stopcode"] = stopcode;
+        departures.push(dep);
+      };
+    };
+    departures.sort(compare);
+    departures = departures.slice(0, NDEPS);
+    //console.log(departures);
+    let htmlout = "<table>";
+    for (i = 0; i < departures.length; i++) {
+      htmlout += renderDepRow(departures[i], nowseconds);
+    };
+    $(".departures").html(htmlout);
+
+  } catch (e) {
+    console.log(e);
+    $(".departures").html('<p class="error">Aikatauluja ei voitu hakea.<br><i>' + e.message + "</i></p>");
+  }
+};
+
+function loadDepartures() {
+  // This function makes the final departures request
+  // and passes the response to formatter functions
+  // that write the result table to the document .departures div
+  let timenow = new Date();
+  let timefrom = Math.floor(timenow.getTime() / 1000) + OFFSET_SEC;
+  let req_actual = REQBODY.replace("START_TIME_PLACEHOLDER", timefrom);
+  //console.log(req_actual); // REMOVETHIS
+
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     let resp = JSON.parse(this.responseText);
+     // TODO pass further to formatter function and show in body
+     renderDepartures(resp);
+     //console.log(resp); // REMOVETHIS
+   }
+  };
+  xhttp.open("POST", ENDPOINT, true);
+  xhttp.setRequestHeader("Content-type", "application/graphql");
+  xhttp.send(req_actual);
+};
+
+function clock() {
+  // Define current HHMMSS clock
+  let time = new Date(),
+  hours = time.getHours(),
+  minutes = time.getMinutes(),
+  seconds = time.getSeconds();
+  $('.clock').html(zpad(hours) + ":" + zpad(minutes) + ":" + zpad(seconds));
+}
+setInterval(clock, 1000);
+
+// Request departures when page loads and then every minute
+function ajaxd_timed() {
+    let time = new Date(),
+    seconds = time.getSeconds()
+    if (seconds == 0) {
+      loadDepartures();
+    }
+}
+$(document).ready(function() {
+    loadDepartures();
+    setInterval(ajaxd_timed, 1000);
+});
